de539887ad8ff73dfcdba787a5c76f4aedb5114f
ExGeneralTz <49422055+ExGeneralTz@users.noreply.github.com>
2019-05-30 17:21:11 +0700

    Add files via upload

---
+++ install.py
@@ -0,0 +1,10 @@
+#intalling
+
+import os
+
+os.system('pkg update')
+os.system('pkg upgarde')
+os.system('pkg install php')
+os.system('pkg install python2')
+os.system('pip2 install requests')
+os.system('pip2 install mechanize')
